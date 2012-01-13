9e20e98b4411fb58c10741cc32962ea0346b0f46
alex <blah.com>
2012-01-12 21:44:48 -0400

    Pushing new changes

---
+++ source/app.specs/EnumerableExtensionsSpecs.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+using app.utility;
+using developwithpassion.specifications.rhinomocks;
+using developwithpassion.specifications.extensions;
+
+namespace app.specs
+{
+    [Subject(typeof (EnumerableExtensions))]
+    public class EnumerableExtensionsSpecs
+    {
+        public abstract class concern : Observes
+        {
+
+        }
+
+        public class when_iterating_on_a_collection_of_items : concern
+        {
+            Establish context = () =>
+            {
+                list = Enumerable.Range(1, 100).Select(x=>new TheClass()).ToList();
+
+            };
+
+            Because of = () => list.for_each(x => x.was_called = true);
+            
+            It should_return_an_enumerable_type = () => list.ShouldEachConformTo(x => x.was_called);
+
+            static IEnumerable<TheClass> list;
+                   
+        }
+
+        public class TheClass
+        {
+            public bool was_called { get; set; }
+        }
+    }
+}
---
+++ source/app/utility/EnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace app.utility
+{
+    public static class EnumerableExtensions
+    {
+        public static void for_each<Type>(this IEnumerable<Type> items,Action<Type> action)
+        {
+            foreach (var item in items)
+            {
+                action(item);
+            }
+        }
+    }
+}
